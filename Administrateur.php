a92317043f92f702a24b65c03416d5093f4ca6e7
anass1110 <anass2006@icloud.com>
2026-04-05 23:45:39 +0200

    Update Administrateur.php

--- Administrateur.php
+++ Administrateur.php
@@ -1,10 +1,12 @@
 <?php
+// Vérification des droits d'accès (Contrôle de rôle)
 session_start();
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
     header("Location: accueil.php");
     exit();
 }
 
+// Extraction du référentiel des utilisateurs
 $fichier_json = 'data/utilisateurs.json';
 $utilisateurs = [];
 if(file_exists($fichier_json)){
@@ -35,18 +37,20 @@ if(file_exists($fichier_json)){
             <tbody>
                  <?php foreach ($utilisateurs as $u): ?>
                     <tr class="user-row">
-                 <td><?= htmlspecialchars($u['id']) ?></td>
-                 <td><?= htmlspecialchars($u['informations']['nom'] . ' ' . $u['informations']['prenom']) ?></td>
-                 <td><?= htmlspecialchars($u['role']) ?></td>
-        
-                  <td>
-                 <button class="bouton-nav" style="color: red; border-color: red;">Bloquer</button>
-                 <select class="bouton-nav">
-                        <option value="standard">Standard</option>
-                        <option value="vip">VIP</option>
-                     </select>
-                     <a href="#" class="bouton-nav">Voir Profil</a>
-                 </td> </tr> <?php endforeach; ?>
+                         <td><?= htmlspecialchars($u['id']) ?></td>
+                         <td><?= htmlspecialchars($u['informations']['nom'] . ' ' . $u['informations']['prenom']) ?></td>
+                         <td><?= htmlspecialchars($u['role']) ?></td>
+                
+                         <td>
+                             <button class="bouton-nav" style="color: red; border-color: red;">Bloquer</button>
+                             <select class="bouton-nav">
+                                 <option value="standard">Standard</option>
+                                 <option value="vip">VIP</option>
+                             </select>
+                             <a href="Profil.php?id=<?= urlencode($u['id']) ?>" class="bouton-nav">Voir Profil</a>
+                         </td> 
+                    </tr> 
+                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
