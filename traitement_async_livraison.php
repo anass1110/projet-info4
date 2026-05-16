f91ddf54bc2620a50d2342f44dd331c9c6e45a98
anass1110 <anass2006@icloud.com>
2026-05-16 17:59:20 +0200

    Update traitement_async_livraison.php

--- traitement_async_livraison.php
+++ traitement_async_livraison.php
@@ -5,11 +5,14 @@ header('Content-Type: application/json');
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'livreur') {
     echo json_encode(["success" => false]); exit();
 }
+
 if (isset($_POST['id_commande']) && isset($_POST['action'])) {
     $fichier = 'data/commandes.json';
     $data = json_decode(file_get_contents($fichier), true);
+    
     foreach ($data['commandes'] as $i => $c) {
-        if ($c['id_commande'] === $_POST['id_commande']) {
+        // Double égal (==)
+        if ($c['id_commande'] == $_POST['id_commande']) {
             if ($_POST['action'] === 'valider') {
                 $data['commandes'][$i]['statut'] = 'Livrée';
             } elseif ($_POST['action'] === 'abandonner') {
