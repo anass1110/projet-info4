a2c6938820879ea23a0289f4515434da36520697
anass1110 <anass2006@icloud.com>
2026-05-16 16:54:48 +0200

    Update traitement_async_panier.php

--- traitement_async_panier.php
+++ traitement_async_panier.php
@@ -4,25 +4,39 @@ header('Content-Type: application/json');
 
 if (!isset($_SESSION['panier'])) { $_SESSION['panier'] = []; }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] == 'ajouter') {
-    $trouve = false;
-    for ($i = 0; $i < count($_SESSION['panier']); $i++) {
-        if ($_SESSION['panier'][$i]['id'] === $_POST['id_article']) {
-            $_SESSION['panier'][$i]['quantite'] += intval($_POST['quantite']);
-            $trouve = true; break; 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
+    
+    // CAS 1 : AJOUTER
+    if ($_POST['action'] === 'ajouter') {
+        $trouve = false;
+        for ($i = 0; $i < count($_SESSION['panier']); $i++) {
+            if ($_SESSION['panier'][$i]['id'] === $_POST['id_article']) {
+                $_SESSION['panier'][$i]['quantite'] += intval($_POST['quantite']);
+                $trouve = true; break; 
+            }
         }
+        if (!$trouve) {
+            $_SESSION['panier'][] = [
+                'id' => $_POST['id_article'],
+                'nom' => $_POST['nom_article'],
+                'prix' => floatval($_POST['prix']),
+                'quantite' => intval($_POST['quantite']),
+                'option' => $_POST['option_choisie'] ?? ''
+            ];
+        }
+        echo json_encode(["success" => true, "action" => "ajouter"]); exit();
     }
-    if (!$trouve) {
-        $_SESSION['panier'][] = [
-            'id' => $_POST['id_article'],
-            'nom' => $_POST['nom_article'],
-            'prix' => floatval($_POST['prix']),
-            'quantite' => intval($_POST['quantite']),
-            'option' => $_POST['option_choisie'] ?? ''
-        ];
+
+    // CAS 2 : SUPPRIMER
+    if ($_POST['action'] === 'supprimer') {
+        if (isset($_POST['index_article'])) {
+            $index = intval($_POST['index_article']);
+            if (isset($_SESSION['panier'][$index])) {
+                array_splice($_SESSION['panier'], $index, 1);
+            }
+        }
+        echo json_encode(["success" => true, "action" => "supprimer"]); exit();
     }
-    echo json_encode(["success" => true]);
-    exit();
 }
 echo json_encode(["success" => false]);
 ?>
