61f1a49309e6853f0bfb305889c70b921515bf15
anass1110 <anass2006@icloud.com>
2026-05-16 17:59:50 +0200

    Update traitement_async_admin.php

--- traitement_async_admin.php
+++ traitement_async_admin.php
@@ -5,9 +5,20 @@ header('Content-Type: application/json');
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
     echo json_encode(["success" => false]); exit();
 }
+
 if (isset($_POST['id_user']) && $_POST['action'] === 'bloquer') {
-    echo json_encode(["success" => true]);
-    exit();
+    $fichier = 'data/utilisateurs.json';
+    $data = json_decode(file_get_contents($fichier), true);
+    
+    foreach ($data['utilisateurs'] as $i => $u) {
+        //  Double égal (==) 
+        if ($u['id'] == $_POST['id_user']) {
+            $data['utilisateurs'][$i]['statut'] = 'bloque';
+            file_put_contents($fichier, json_encode($data, JSON_PRETTY_PRINT));
+            echo json_encode(["success" => true]);
+            exit();
+        }
+    }
 }
 echo json_encode(["success" => false]);
 ?>
